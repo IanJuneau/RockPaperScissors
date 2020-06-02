93e404aaa3c28410e8889e2103d3266e6aa751c0
Kepler <kyplerr@gmail.com>
2020-06-01 22:25:58 -0700

    Removed JFrame code from Window constructor.

--- src/Graphics/Window.java
+++ src/Graphics/Window.java
@@ -11,12 +11,7 @@ public class Window extends Canvas {
     //}
 
     public Window(){
-        JFrame frame = new JFrame("Name");
         Window window = new Window();
-        frame.setSize(600,400);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(window);
     }
 
 }
